c63591066fb3af004f140a1f5d97a227a138315d
Rafael Vasconcelos <rafael.vasconcelos7497@gmail.com>
2026-02-21 01:10:28 +0000

    Update inference models

--- src/Engine/hf-inference.ts
+++ src/Engine/hf-inference.ts
@@ -23,7 +23,8 @@ class MissingInferenceAPIKeyException extends Error {
 
 class InferenceClient extends HfInference { 
     public static InferenceModels: Record<string, InferenceProvider> = { 
-        "openai/gpt-oss-120b": "fireworks-ai",
+        "MiniMaxAI/MiniMax-M2.1": "novita",
+        "MiniMaxAI/MiniMax-M2.5": "novita",
 
         "deepseek-ai/DeepSeek-R1": "fireworks-ai",
         "deepseek-ai/DeepSeek-R1-0528": "fireworks-ai",
@@ -34,26 +35,29 @@ class InferenceClient extends HfInference {
         "deepseek-ai/DeepSeek-V3.2-Exp": "novita",
         "deepseek-ai/DeepSeek-V3.2": "novita",
 
+        "Qwen/Qwen2.5-72B-Instruct": "hf-inference",
         "Qwen/Qwen3-Coder-480B-A35B-Instruct": "novita",
         "Qwen/Qwen3-Next-80B-A3B-Instruct": "novita",
         "Qwen/Qwen3-Next-80B-A3B-Thinking": "novita",
         "Qwen/Qwen3-235B-A22B-Thinking-2507": "novita",
         "Qwen/Qwen3-235B-A22B-Instruct-2507": "novita",
-        "Qwen/Qwen2.5-72B-Instruct": "hf-inference",
+        "Qwen/Qwen3.5-397B-A17B": "novita",
 
         "moonshotai/Kimi-K2-Instruct": "groq",
         "moonshotai/Kimi-K2-Instruct-0905": "novita",
         "moonshotai/Kimi-K2-Thinking": "novita",
+        "moonshotai/Kimi-K2.5": "novita",
 
         "zai-org/GLM-4.5-Air": "fireworks-ai",
         "zai-org/GLM-4.5": "fireworks-ai",
         "zai-org/GLM-4.6": "novita",
         "zai-org/GLM-4.7": "novita",
+        "zai-org/GLM-5": "novita",
 
-        "MiniMaxAI/MiniMax-M2.1": "novita",
         "XiaomiMiMo/MiMo-V2-Flash": "novita",
         "CohereLabs/command-a-translate-08-2025": "cohere",
 
+        "openai/gpt-oss-120b": "fireworks-ai",
         "google/gemma-3-27b-it": "hf-inference",
         "meta-llama/Llama-3.3-70B-Instruct": "fireworks-ai",
         "shisa-ai/shisa-v2-llama3.3-70b": "featherless-ai",
